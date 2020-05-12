85c2c6aaf1fed4003015999ee1b52a4e8395a7b5
tarkesh2shar <tarkesh2shar@gmail.com>
2020-05-12 18:18:10 +0530

    hell yeah

---
+++ src/components/App.js
@@ -0,0 +1,56 @@
+import React, { lazy, Suspense } from "react"
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom"
+import "../index.scss"
+import "../index.css"
+
+//import Test from './sample/test'
+//Testing Lazy Loading
+
+const Test = lazy(() => import("./sample/test"))
+
+export default function App() {
+	function WaitingComponent(Component) {
+		return props => (
+			<Suspense fallback={<div>Loading...</div>}>
+				<Component {...props} />
+			</Suspense>
+		)
+	}
+
+	return (
+		<div className="container mt-2">
+			<BrowserRouter>
+				<div className="routes">
+					<Link to="/add">Route(/add)</Link>
+					<Link to="/add/1">Route(/add/1)</Link>
+					<Link to="/add/2">Route(/add/2)</Link>
+					<Link to="/test">Route(test) --> using code splitting</Link>
+					<Link to="/test">Route(test) --> UPdate to aws</Link>
+				</div>
+				<Switch>
+					<Route
+						path="/add"
+						component={() => {
+							return (
+								<React.Fragment>
+									headerr
+									<br />
+									<Switch>
+										<Route exact component={() => "This is path ---> /add/2"} path="/add/2" />
+										<Route exact component={() => "This is path ---> /add/1"} path="/add/1" />
+										<Route component={() => "This is path ---> /add"} />
+									</Switch>
+									<br />
+									footer
+								</React.Fragment>
+							)
+						}}
+					/>
+					<Route component={WaitingComponent(Test)} path="/test" />
+					<Route path="/" exact component={() => "This is path ---> /"} />
+					<Route component={() => "Page NOT foUND"} />
+				</Switch>
+			</BrowserRouter>
+		</div>
+	)
+}
---
+++ webpack.dev.js
@@ -0,0 +1,22 @@
+const merge = require('webpack-merge')
+const common = require('./webpack.common')
+const path = require('path')
+
+module.exports = merge(common, {
+	mode: 'development',
+	devtool: 'inline-source-map',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].[hash].js',
+		publicPath: '/',
+	},
+	devServer: {
+		contentBase: './dist',
+		hot: true,
+		historyApiFallback: true,
+		publicPath: '/',
+		proxy: {
+			'/api': 'http://localhost:2000',
+		},
+	},
+})
